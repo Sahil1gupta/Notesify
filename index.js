cf22c24ed3d4044ab5f77f70c1eeffd00e65f0c5
Sahil1gupta <ej1905630411@gmail.com>
2023-12-12 02:39:21 +0530

    Axios Path changed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-import axios from '.node_modules/axios';
+import axios from '/node_modules/axios';
 console.log("index included")
 console.log("running")
 const audiofil=document.getElementById('audiofile').value;
