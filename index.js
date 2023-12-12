18c1e3bd0b6226ed37afed5c758687fb4f0ee7cf
Rohit Pal <prohit9370@gmail.com>
2023-12-12 13:59:11 +0530

    .././.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 console.log("index included");
 console.log("running");
-const audiofil = document.getElementById("audio").value;
-console.log(audiofil);
+const audiofile = document.getElementById("audiofile").value;
+console.log(audiofile);
 const form = document.getElementById("uploadAudio");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const formData = new FormData();
-  const audio = document.getElementById("audio").files[0];
-  formData.append("audio", audio);
+  const audiofile = document.getElementById("audiofile").files[0];
+  formData.append("audiofile", audiofile);
 
   try {
     const response = await axios.post(
-      "https://notesify-server.vercel.app/speech/generateSpeech",
+      "https://notesify-server.vercel.app/transcript/transcriptAudio",
       formData,
       {
         headers: {
