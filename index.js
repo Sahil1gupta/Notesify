7caf82e4494c6358b1fd10ba0e2d1283b574bd92
Rohit Pal <prohit9370@gmail.com>
2023-12-12 13:06:00 +0530

    changed audiofile to audio

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 console.log("index included");
 console.log("running");
-const audiofil = document.getElementById("audiofile").value;
+const audiofil = document.getElementById("audio").value;
 console.log(audiofil);
 const form = document.getElementById("uploadAudio");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const formData = new FormData();
-  const audiofile = document.getElementById("audiofile").files[0];
-  formData.append("audiofile", audiofile);
+  const audio = document.getElementById("audio").files[0];
+  formData.append("audio", audio);
 
   try {
     const response = await axios.post(
@@ -21,7 +21,7 @@ form.addEventListener("submit", async (event) => {
           "Accept-Encoding": "gzip, deflate, br",
           Connection: "keep-alive",
           authorization:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NzVhYTFjNTk4ZWJlMmQyNGVhNTZkMyIsImlhdCI6MTcwMjMxOTgxNiwiZXhwIjoxNzAyMzQxNDE2fQ.9ABNiNpy92eQ5jNv_cBgT5GTzdcJaiJC6rwLG0_A7Jg"
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NzVhYTFjNTk4ZWJlMmQyNGVhNTZkMyIsImlhdCI6MTcwMjM2NjUxMywiZXhwIjoxNzAyMzg4MTEzfQ.oGQKtkBTeiDYJup1alONQKd4t8-ocJnFc_bANS7R2Xw"
         }
       }
     );
