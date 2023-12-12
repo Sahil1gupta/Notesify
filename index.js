e4c24163326b8cd8a1aae89e8b5c392c116c7cb9
Rohit Pal <prohit9370@gmail.com>
2023-12-12 13:43:46 +0530

    added transcript

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ form.addEventListener("submit", async (event) => {
 
   try {
     const response = await axios.post(
-      "https://notesify-server.vercel.app/translate/translateAudio",
+      "https://notesify-server.vercel.app/transcript/transcriptAudio",
       formData,
       {
         headers: {
