7eb6353fa195a69ac3f56ae339272951734ac1d8
Sahil1gupta <ej1905630411@gmail.com>
2024-02-22 22:40:36 +0530

    text changes

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
+import axios from 'axios'; // Import axios library
+
 const form=document.getElementById("registerForm");
 
-form.addEventListener('submit',async(event)=>{
-
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
-    let RegisterData=new FormData();
+    let RegisterData = new FormData();
 
-    let firstname=document.getElementById('firstname').value
-    let lastname=document.getElementById('lastname').value
-    let email=document.getElementById('email').value
-    let username=document.getElementById('username').value
-    let password=document.getElementById('password').value
+    let firstname = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let email = document.getElementById('email').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
 
-    RegisterData.append("firstname",firstname);
-    RegisterData.append("lastname",lastname);
-    RegisterData.append("email",email);
-    RegisterData.append("username",username);
-    RegisterData.append("password",password);
+    RegisterData.append("firstname", firstname);
+    RegisterData.append("lastname", lastname);
+    RegisterData.append("email", email);
+    RegisterData.append("username", username);
+    RegisterData.append("password", password);
 
-    try{
-        const response=await axios.post("https://notesify-server.vercel.app//user/signup",RegisterData,
-        {
+    try {
+        const response = await axios.post("https://notesify-server.vercel.app/user/signup", RegisterData, {
             headers: {
                 // authorization: "ACCESS_TOKEN"  
             },
             // responseType: "json"
-          }
-        )
-          const jwtToken=await response.data;
-          console.log(jwtToken)
-        window.location.href="login.html"
-    }
-    catch (error) {
+        });
+        const jwtToken = await response.data;
+        console.log(jwtToken);
+        window.location.href = "login.html";
+    } catch (error) {
         console.error("Error:", error);
         // Handle registration error here
     }
-})
+});
