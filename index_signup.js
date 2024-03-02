9764df62337653cf5c6511c4c2c521adb32b7cdd
Rohit Pal <prohit9370@gmail.com>
2024-03-03 01:42:31 +0530

    changes in script tag

--- index_signup.js
+++ index_signup.js
@@ -1,39 +1,48 @@
 // import axios from 'axios'; // Import axios library
 
-const form=document.getElementById("registerForm");
+const form = document.getElementById("registerForm");
 
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    let RegisterData = new FormData();
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  let RegisterData = new FormData();
 
-    let firstName = document.getElementById('firstName').value;
-    let lastName = document.getElementById('lastName').value;
-    let userName = document.getElementById('userName').value;
-    let email = document.getElementById('email').value;
-   
-    let password = document.getElementById('password').value;
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let userName = document.getElementById("userName").value;
+  let email = document.getElementById("email").value;
 
-    RegisterData.append("firstName", firstName);
-    RegisterData.append("lastName", lastName);
-    RegisterData.append("userName", userName);
-    RegisterData.append("email", email);
-    RegisterData.append("password", password);
+  let password = document.getElementById("password").value;
 
-    try {
-        const response = await axios.post("https://notesify-server.vercel.app/user/signup", RegisterData, {
-            headers: {
-                // authorization: "ACCESS_TOKEN"  
-            },
-            // responseType: "json"
-        });
-        if(!response.ok){
-            console.log("network issue")
-        }
-        const jwtToken =  response.data;
-        console.log(jwtToken);
-        window.location.href = "login.html";
-    } catch (error) {
-        console.error("Error:", error);
-        // Handle registration error here
+  RegisterData.append("firstName", firstName);
+  RegisterData.append("lastName", lastName);
+  RegisterData.append("userName", userName);
+  RegisterData.append("email", email);
+  RegisterData.append("password", password);
+
+  try {
+    const url = "https://notesify-server.vercel.app/user/signup";
+    const method = "post";
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer null",
+    };
+
+    const data = RegisterData;
+
+    const responseType = "json";
+
+    const response = await axios({ url, method, headers, data, responseType });
+
+    console.log(response);
+    if (response.status !== 200) {
+      console.log("network issue");
+    } else {
+      const jwtToken = response.data;
+      console.log(jwtToken);
+      window.location.href = "login.html";
     }
+  } catch (error) {
+    console.error("Error:", error);
+    // Handle registration error here
+  }
 });
