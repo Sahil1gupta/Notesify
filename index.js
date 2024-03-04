5eef2466e8957969669ddf6dd49983100f113a2a
Rohit Pal <prohit9370@gmail.com>
2024-03-04 15:09:31 +0530

    consoling cookie

--- index.js
+++ index.js
@@ -31,13 +31,21 @@ form.addEventListener("submit", async (event) => {
 
     const responseType = "json";
 
-    const response = await axios({ url, method, headers, data, responseType });
+    const response = await axios({
+      url,
+      method,
+      headers,
+      data,
+      responseType,
+      withCredentials: true,
+    });
 
     console.log(response);
     if (response.status !== 200) {
       console.log("network issue");
     } else {
-        // console.log(response.data);
+      console.log(response.cookie);
+      // console.log(response.data);
       // const jwtToken = response.data.token;
       // document.cookie = `token=${jwtToken}; path=/`;
       // console.log(jwtToken);
