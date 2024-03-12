689eb43c2e3756aa91545f305b27eeb09ade7d3b
Rohit Pal <prohit9370@gmail.com>
2024-03-13 01:51:56 +0530

    Added placeholder in quill js

--- quillSetup.js
+++ quillSetup.js
@@ -1,34 +1,34 @@
-
-
- document.addEventListener('DOMContentLoaded', () => {
-    // Your script here
-    var quill = new Quill('#editor', {
-        theme: 'snow',
-        modules: {
-         toolbar: {
-           container: [
-             ['bold', 'italic', 'underline', 'strike'],  // Existing options
-             [{ 'background': [] }, { 'color': [] }],   // Background and text color options
-             ['link', 'image', 'video'],                // Existing options
-             [{ 'header': [1, 2, 3, 4, 5, 6, false] }], // Existing options
-             [{ 'list': 'ordered' }, { 'list': 'bullet' }], // Existing options
-             ['code-block'],                            // Existing options
-             ['highlight']                              // New highlight option
-           ],
-           handlers: {
-             'highlight': function () {
-               var selection = quill.getSelection();
-               if (selection) {
-                 var range = quill.getSelection(true);
-                 quill.formatText(range.index, range.length, 'background', 'yellow');
-               }
-             }
-           }
-         }
-       }
-     });
-     
-     
-    
+document.addEventListener("DOMContentLoaded", () => {
+  // Your script here
+  var quill = new Quill("#editor", {
+    theme: "snow",
+    placeholder: "Audio to Text output will be shown here...",
+    modules: {
+      toolbar: {
+        container: [
+          ["bold", "italic", "underline", "strike"], // Existing options
+          [{ background: [] }, { color: [] }], // Background and text color options
+          ["link", "image", "video"], // Existing options
+          [{ header: [1, 2, 3, 4, 5, 6, false] }], // Existing options
+          [{ list: "ordered" }, { list: "bullet" }], // Existing options
+          ["code-block"], // Existing options
+          ["highlight"], // New highlight option
+        ],
+        handlers: {
+          highlight: function () {
+            var selection = quill.getSelection();
+            if (selection) {
+              var range = quill.getSelection(true);
+              quill.formatText(
+                range.index,
+                range.length,
+                "background",
+                "yellow"
+              );
+            }
+          },
+        },
+      },
+    },
   });
-  
+});
